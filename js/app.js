73f37eb6ab4195516672ff42e098994562d2163d
Victor Bruce <victorbruce82@gmail.com>
2025-04-16 13:01:20 +0000

    feat: get the character count from the textarea field

--- js/app.js
+++ js/app.js
@@ -12,8 +12,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // toggle theme
 document.getElementById("theme-toggle").addEventListener("click", () => {
-  // alert("Hello theming!");
   const currentTheme = document.documentElement.dataset.theme;
   const newTheme = currentTheme === "dark" ? "light" : "dark";
   applyTheme(newTheme);
 });
+
+const characterCountElement = document.querySelector(
+  ".cards__card--characters .card__count"
+);
+
+function textareaInput() {
+  // get the textarea input field
+  const textarea = document.getElementById("textareaField");
+  textarea.addEventListener("input", () => {
+    const characterCount = textarea.value.length;
+
+    characterCountElement.textContent =
+      characterCount === 0 ? "00" : characterCount;
+  });
+}
+
+
+textareaInput();
